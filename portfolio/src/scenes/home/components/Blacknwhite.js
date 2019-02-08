c6f014c30bb5cca3424304342f823e194a94da0a
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-02-08 15:53:31 -0500

    blacknwhite component

---
+++ portfolio/src/scenes/home/components/Blacknwhite.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import vectorlines1 from '../images/vectorlines1.svg';
+
+class Blacknwhite extends Component {
+  render() {
+    return (
+      <div className="blacknwhite">
+        <img src= {vectorlines1} className='vectorlines1' />;
+      </div>
+    );
+  }
+}
+export default Blacknwhite;
---
+++ portfolio/src/scenes/home/containers/Homecontainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Blacknwhite from '../components/Blacknwhite.js'
+
+class Homecontainer extends Component {
+  render() {
+    return (
+      <div className="homecontainer">
+        < Blacknwhite />
+        
+      </div>
+    );
+  }
+}
+export default Homecontainer;
