9c53513aa438660aaa429e8a4e8d6c090c0645d1
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-02-11 15:02:03 -0500

    gridlist component finished 6 items

--- portfolio/src/scenes/home/containers/Homecontainer.js
+++ portfolio/src/scenes/home/containers/Homecontainer.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Blacknwhite from '../components/Blacknwhite.js'
 import Colorforest from '../components/Colorforest.js'
+import White from '../components/White.js'
+import Gridlist from '../components/Gridlist.js'
+
+
 
 class Homecontainer extends Component {
   render() {
@@ -8,6 +12,9 @@ class Homecontainer extends Component {
       <div className="homecontainer">
         < Blacknwhite />
         < Colorforest />
+        < White />
+        < Gridlist />
+
 
       </div>
     );
---
+++ portfolio/src/scenes/home/data/tileData.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import imageOne from '../images/gridlist/imageOne.jpg';
+import imageTwo from '../images/gridlist/imageTwo.jpg';
+import imageThree from '../images/gridlist/imageThree.png';
+import imageFour from '../images/gridlist/imageFour.jpg';
+import imageFive from '../images/gridlist/imageFive.jpg';
+import imageSix from '../images/gridlist/imageSix.jpg';
+
+
+const tileData=[
+    {
+        img:imageOne,
+        title: 'first',
+        author: 'blah',
+        cols: 1,
+
+    },
+
+    {
+        img:imageTwo,
+        title: 'second',
+        author:'bloo',
+        cols: 1,
+    },
+
+    {
+        img:imageThree,
+        title: 'third',
+        author: 'bee',
+        cols: 1,
+    },
+
+    {
+        img:imageFour,
+        title: 'fourth',
+        author: 'borg',
+        cols: 1,
+    },
+
+    {
+        img:imageFive,
+        title: 'fifth',
+        author: 'haa',
+        cols: 1,
+    },
+
+    {
+        img:imageSix,
+        title: 'sixth',
+        author: 'hoo',
+        cols: 1,
+    },
+];
+
+export default tileData;
